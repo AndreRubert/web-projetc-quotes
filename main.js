4da48b0e60d19f276d3603c99d41c035a98de0dd
Jake Donfort <andrerubert411@gmail.com>
2023-04-09 14:09:32 -0300

    commit

---
+++ main.js
@@ -0,0 +1,33 @@
+// Slide
+var swiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    loop: true,
+    coverflowEffect: {
+        rotate: 20,
+        stretch: 0,
+        depth: 200,
+        modifier: 0.9,
+        slideShadows: false,
+    },
+});
+
+
+//Slide content
+let cards = document.querySelectorAll(".swiper-slide-content");
+
+for (let card of cards) {
+  card.addEventListener('click', (e) => {
+    card.classList.toggle('flip');
+  });
+}
+
+
+//let card = document.querySelectorAll(".swiper-slide-content");
+
+//card.addEventListener('click', (e) => {
+//    card.classList.toggle('flip')
+//});
+//
